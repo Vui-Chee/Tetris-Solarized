c7ec14809b8091a24fc54e266f33934e9b715fa0
Vui-Chee <vuicheesiew@gmail.com>
2021-06-15 20:03:08 +0800

    Test isMoveValid with simple piece for now

--- __tests__/reducers/blocksReducer/movement.js
+++ __tests__/reducers/blocksReducer/movement.js
@@ -1,4 +1,7 @@
-import { isOutOfBounds } from "../../../src/reducers/blocksReducer/movement.js";
+import {
+  isOutOfBounds,
+  isMoveValid,
+} from "../../../src/reducers/blocksReducer/movement.js";
 
 describe("isOutOfBounds", () => {
   it("coordinates within dimensions", () => {
@@ -16,6 +19,38 @@ describe("isOutOfBounds", () => {
     // Beyond height of box
     expect(isOutOfBounds(18, 0)).toBe(true);
     // NOTE: x can be negative (piece can be above the box)
+    expect(isOutOfBounds(0, -1)).toBe(true);
+  });
+});
+
+describe("isMoveValid", () => {
+  // Z_PIECE_2
+  const blocks = {
+    16: { 3: 1, 4: 1 },
+    17: { 4: 1, 5: 1 },
+  };
+
+  it("Cannot move over piece", () => {
+    expect(isMoveValid(16, 3, blocks)).toBe(false);
+    expect(isMoveValid(16, 4, blocks)).toBe(false);
+    expect(isMoveValid(17, 4, blocks)).toBe(false);
+    expect(isMoveValid(17, 5, blocks)).toBe(false);
+  });
+
+  it("Can move to places outside piece", () => {
+    // Above piece
+    expect(isMoveValid(15, 3, blocks)).toBe(true);
+    expect(isMoveValid(15, 4, blocks)).toBe(true);
+    // On the sides of piece
+    expect(isMoveValid(16, 2, blocks)).toBe(true);
+    expect(isMoveValid(16, 5, blocks)).toBe(true);
+    expect(isMoveValid(17, 3, blocks)).toBe(true);
+    expect(isMoveValid(17, 6, blocks)).toBe(true);
+  });
+
+  it("Cannot move outside dimensions", () => {
+    expect(isOutOfBounds(0, 10)).toBe(true);
+    expect(isMoveValid(18, 0, blocks)).toBe(false);
     expect(isOutOfBounds(0, -1)).toBe(true);
   });
 });
