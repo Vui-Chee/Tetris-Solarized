d793fea1b5656e610c07092713de26c72bd276f2
Vui-Chee <vuicheesiew@gmail.com>
2021-06-20 12:30:27 +0800

    Clearer test descriptions for testUtils
    + reorg some cases.

--- __tests__/blocksReducer/testUtils.js
+++ __tests__/blocksReducer/testUtils.js
@@ -5,22 +5,33 @@ import {
 import { NUM_COLS, NUM_ROWS } from "../../src/utils/constants";
 
 describe("isOutOfBounds", () => {
-  it("coordinates within dimensions", () => {
+  it("should return false for coordinates within the box", () => {
+    // corners
     expect(isOutOfBounds(0, 0)).toBe(false);
     expect(isOutOfBounds(NUM_ROWS - 1, 0)).toBe(false);
     expect(isOutOfBounds(0, NUM_COLS - 1)).toBe(false);
     expect(isOutOfBounds(NUM_ROWS - 1, NUM_COLS - 1)).toBe(false);
+    // edges
     expect(isOutOfBounds(NUM_ROWS - 1, 4)).toBe(false);
     expect(isOutOfBounds(5, NUM_COLS - 1)).toBe(false);
+    // neither edges/corners
+    expect(isOutOfBounds(1, 1)).toBe(false);
+    expect(isOutOfBounds(10, 5)).toBe(false);
   });
 
-  it("coordinates outside dimensions", () => {
-    // Beyond width of box
-    expect(isOutOfBounds(0, NUM_COLS)).toBe(true);
-    // Beyond height of box
-    expect(isOutOfBounds(NUM_ROWS, 0)).toBe(true);
+  it("should return false for coordinates above the box", () => {
     // NOTE: x can be negative (piece can be above the box)
+    expect(isOutOfBounds(-3, 5)).toBe(false);
+    expect(isOutOfBounds(-2, 5)).toBe(false);
+    expect(isOutOfBounds(-1, 5)).toBe(false);
+  });
+
+  it("should return true for coordinates outside the box", () => {
+    // Beyond left/right walls of the box
+    expect(isOutOfBounds(0, NUM_COLS)).toBe(true);
     expect(isOutOfBounds(0, -1)).toBe(true);
+    // Beneath the floor of box
+    expect(isOutOfBounds(NUM_ROWS, 0)).toBe(true);
   });
 });
 
@@ -31,14 +42,14 @@ describe("isMoveValid", () => {
     [NUM_ROWS - 1]: { 4: 1, 5: 1 },
   };
 
-  it("Cannot move over piece", () => {
+  it("should return false for coordinates within blocks", () => {
     expect(isMoveValid(NUM_ROWS - 2, 3, blocks)).toBe(false);
     expect(isMoveValid(NUM_ROWS - 2, 4, blocks)).toBe(false);
     expect(isMoveValid(NUM_ROWS - 1, 4, blocks)).toBe(false);
     expect(isMoveValid(NUM_ROWS - 1, 5, blocks)).toBe(false);
   });
 
-  it("Can move to places outside piece", () => {
+  it("should return true for coordinates outside blocks", () => {
     // Above piece
     expect(isMoveValid(15, 3, blocks)).toBe(true);
     expect(isMoveValid(15, 4, blocks)).toBe(true);
@@ -49,7 +60,7 @@ describe("isMoveValid", () => {
     expect(isMoveValid(NUM_ROWS - 1, 6, blocks)).toBe(true);
   });
 
-  it("Cannot move outside dimensions", () => {
+  it("should return false for coordinates outside the box", () => {
     expect(isOutOfBounds(0, NUM_COLS)).toBe(true);
     expect(isMoveValid(NUM_ROWS, 0, blocks)).toBe(false);
     expect(isOutOfBounds(0, -1)).toBe(true);
