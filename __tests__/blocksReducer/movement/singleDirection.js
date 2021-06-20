01bcdd28809e34eb5f3b7a0f414ae2a3635d34a1
Vui-Chee <vuicheesiew@gmail.com>
2021-06-20 17:13:19 +0800

    Test movement for L_PIECE_1 in all orientations

--- __tests__/blocksReducer/movement/singleDirection.js
+++ __tests__/blocksReducer/movement/singleDirection.js
@@ -15,37 +15,261 @@ import {
 import { testMove } from "../utils";
 
 describe("Can move piece left,right,down (no combine blocks)", () => {
-  it("L_PIECE_1", () => {
-    testMove(
-      L_PIECE_1,
-      [LEFT_KEYCODE],
-      [
-        { x: -1, y: 2, color: 0, type: 0 },
-        { x: -1, y: 3, color: 0, type: 1 },
-        { x: -2, y: 3, color: 0, type: 2 },
-        { x: -3, y: 3, color: 0, type: 1 },
-      ]
-    );
-    testMove(
-      L_PIECE_1,
-      [RIGHT_KEYCODE],
-      [
-        { x: -1, y: 4, color: 0, type: 0 },
-        { x: -1, y: 5, color: 0, type: 1 },
-        { x: -2, y: 5, color: 0, type: 2 },
-        { x: -3, y: 5, color: 0, type: 1 },
-      ]
-    );
-    testMove(
-      L_PIECE_1,
-      [DOWN_KEYCODE],
-      [
-        { x: 0, y: 3, color: 0, type: 0 },
-        { x: 0, y: 4, color: 0, type: 1 },
-        { x: -1, y: 4, color: 0, type: 2 },
-        { x: -2, y: 4, color: 0, type: 1 },
-      ]
-    );
+  describe("L_PIECE_1", () => {
+    describe("can move left", () => {
+      it("in orientation 0", () => {
+        testMove(
+          L_PIECE_1, // default
+          [LEFT_KEYCODE],
+          [
+            { x: -1, y: 2, color: 0, type: 0 },
+            { x: -1, y: 3, color: 0, type: 1 },
+            { x: -2, y: 3, color: 0, type: 2 },
+            { x: -3, y: 3, color: 0, type: 1 },
+          ]
+        );
+      });
+      it("in orientation 1", () => {
+        testMove(
+          {
+            type: "TYPE_L_PIECE_1",
+            orientation: 1,
+            blocks: [
+              { x: -3, y: 3, color: 0, type: 0 },
+              { x: -2, y: 3, color: 0, type: 1 },
+              { x: -2, y: 4, color: 0, type: 2 },
+              { x: -2, y: 5, color: 0, type: 1 },
+            ],
+          },
+          [LEFT_KEYCODE],
+          [
+            { x: -3, y: 2, color: 0, type: 0 },
+            { x: -2, y: 2, color: 0, type: 1 },
+            { x: -2, y: 3, color: 0, type: 2 },
+            { x: -2, y: 4, color: 0, type: 1 },
+          ]
+        );
+      });
+      it("in orientation 2", () => {
+        testMove(
+          {
+            type: "TYPE_L_PIECE_1",
+            orientation: 2,
+            blocks: [
+              { x: -3, y: 5, color: 0, type: 0 },
+              { x: -3, y: 4, color: 0, type: 1 },
+              { x: -2, y: 4, color: 0, type: 2 },
+              { x: -1, y: 4, color: 0, type: 1 },
+            ],
+          },
+          [LEFT_KEYCODE],
+          [
+            { x: -3, y: 4, color: 0, type: 0 },
+            { x: -3, y: 3, color: 0, type: 1 },
+            { x: -2, y: 3, color: 0, type: 2 },
+            { x: -1, y: 3, color: 0, type: 1 },
+          ]
+        );
+      });
+      it("in orientation 3", () => {
+        testMove(
+          {
+            type: "TYPE_L_PIECE_1",
+            orientation: 3,
+            blocks: [
+              { x: -1, y: 5, color: 0, type: 0 },
+              { x: -2, y: 5, color: 0, type: 1 },
+              { x: -2, y: 4, color: 0, type: 2 },
+              { x: -2, y: 3, color: 0, type: 1 },
+            ],
+          },
+          [LEFT_KEYCODE],
+          [
+            { x: -1, y: 4, color: 0, type: 0 },
+            { x: -2, y: 4, color: 0, type: 1 },
+            { x: -2, y: 3, color: 0, type: 2 },
+            { x: -2, y: 2, color: 0, type: 1 },
+          ]
+        );
+      });
+    });
+
+    describe("can move right", () => {
+      it("in orientation 0", () => {
+        testMove(
+          {
+            type: "TYPE_L_PIECE_1",
+            orientation: 0,
+            blocks: [
+              { x: -1, y: 3, color: 0, type: 0 },
+              { x: -1, y: 4, color: 0, type: 1 },
+              { x: -2, y: 4, color: 0, type: 2 },
+              { x: -3, y: 4, color: 0, type: 1 },
+            ],
+          },
+          [RIGHT_KEYCODE],
+          [
+            { x: -1, y: 4, color: 0, type: 0 },
+            { x: -1, y: 5, color: 0, type: 1 },
+            { x: -2, y: 5, color: 0, type: 2 },
+            { x: -3, y: 5, color: 0, type: 1 },
+          ]
+        );
+      });
+
+      it("in orientation 1", () => {
+        testMove(
+          {
+            type: "TYPE_L_PIECE_1",
+            orientation: 1,
+            blocks: [
+              { x: -3, y: 3, color: 0, type: 0 },
+              { x: -2, y: 3, color: 0, type: 1 },
+              { x: -2, y: 4, color: 0, type: 2 },
+              { x: -2, y: 5, color: 0, type: 1 },
+            ],
+          },
+          [RIGHT_KEYCODE],
+          [
+            { x: -3, y: 4, color: 0, type: 0 },
+            { x: -2, y: 4, color: 0, type: 1 },
+            { x: -2, y: 5, color: 0, type: 2 },
+            { x: -2, y: 6, color: 0, type: 1 },
+          ]
+        );
+      });
+
+      it("in orientation 2", () => {
+        testMove(
+          {
+            type: "TYPE_L_PIECE_1",
+            orientation: 2,
+            blocks: [
+              { x: -3, y: 5, color: 0, type: 0 },
+              { x: -3, y: 4, color: 0, type: 1 },
+              { x: -2, y: 4, color: 0, type: 2 },
+              { x: -1, y: 4, color: 0, type: 1 },
+            ],
+          },
+          [RIGHT_KEYCODE],
+          [
+            { x: -3, y: 6, color: 0, type: 0 },
+            { x: -3, y: 5, color: 0, type: 1 },
+            { x: -2, y: 5, color: 0, type: 2 },
+            { x: -1, y: 5, color: 0, type: 1 },
+          ]
+        );
+      });
+
+      it("in orientation 3", () => {
+        testMove(
+          {
+            type: "TYPE_L_PIECE_1",
+            orientation: 3,
+            blocks: [
+              { x: -1, y: 5, color: 0, type: 0 },
+              { x: -2, y: 5, color: 0, type: 1 },
+              { x: -2, y: 4, color: 0, type: 2 },
+              { x: -2, y: 3, color: 0, type: 1 },
+            ],
+          },
+          [RIGHT_KEYCODE],
+          [
+            { x: -1, y: 6, color: 0, type: 0 },
+            { x: -2, y: 6, color: 0, type: 1 },
+            { x: -2, y: 5, color: 0, type: 2 },
+            { x: -2, y: 4, color: 0, type: 1 },
+          ]
+        );
+      });
+    });
+
+    describe("can move down", () => {
+      it("in orientation 0", () => {
+        testMove(
+          {
+            type: "TYPE_L_PIECE_1",
+            orientation: 0,
+            blocks: [
+              { x: -1, y: 3, color: 0, type: 0 },
+              { x: -1, y: 4, color: 0, type: 1 },
+              { x: -2, y: 4, color: 0, type: 2 },
+              { x: -3, y: 4, color: 0, type: 1 },
+            ],
+          },
+          [DOWN_KEYCODE],
+          [
+            { x: 0, y: 3, color: 0, type: 0 },
+            { x: 0, y: 4, color: 0, type: 1 },
+            { x: -1, y: 4, color: 0, type: 2 },
+            { x: -2, y: 4, color: 0, type: 1 },
+          ]
+        );
+      });
+      it("in orientation 1", () => {
+        testMove(
+          {
+            type: "TYPE_L_PIECE_1",
+            orientation: 1,
+            blocks: [
+              { x: -3, y: 3, color: 0, type: 0 },
+              { x: -2, y: 3, color: 0, type: 1 },
+              { x: -2, y: 4, color: 0, type: 2 },
+              { x: -2, y: 5, color: 0, type: 1 },
+            ],
+          },
+          [DOWN_KEYCODE],
+          [
+            { x: -2, y: 3, color: 0, type: 0 },
+            { x: -1, y: 3, color: 0, type: 1 },
+            { x: -1, y: 4, color: 0, type: 2 },
+            { x: -1, y: 5, color: 0, type: 1 },
+          ]
+        );
+      });
+      it("in orientation 2", () => {
+        testMove(
+          {
+            type: "TYPE_L_PIECE_1",
+            orientation: 2,
+            blocks: [
+              { x: -3, y: 5, color: 0, type: 0 },
+              { x: -3, y: 4, color: 0, type: 1 },
+              { x: -2, y: 4, color: 0, type: 2 },
+              { x: -1, y: 4, color: 0, type: 1 },
+            ],
+          },
+          [DOWN_KEYCODE],
+          [
+            { x: -2, y: 5, color: 0, type: 0 },
+            { x: -2, y: 4, color: 0, type: 1 },
+            { x: -1, y: 4, color: 0, type: 2 },
+            { x: 0, y: 4, color: 0, type: 1 },
+          ]
+        );
+      });
+      it("in orientation 3", () => {
+        testMove(
+          {
+            type: "TYPE_L_PIECE_1",
+            orientation: 3,
+            blocks: [
+              { x: -1, y: 5, color: 0, type: 0 },
+              { x: -2, y: 5, color: 0, type: 1 },
+              { x: -2, y: 4, color: 0, type: 2 },
+              { x: -2, y: 3, color: 0, type: 1 },
+            ],
+          },
+          [DOWN_KEYCODE],
+          [
+            { x: 0, y: 5, color: 0, type: 0 },
+            { x: -1, y: 5, color: 0, type: 1 },
+            { x: -1, y: 4, color: 0, type: 2 },
+            { x: -1, y: 3, color: 0, type: 1 },
+          ]
+        );
+      });
+    });
   });
 
   it("L_PIECE_2", () => {
